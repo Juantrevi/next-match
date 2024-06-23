dc769d00714d4190f5cadfca1a71c5d40130513d
Juantrevi <juantrevi70@gmail.com>
2024-06-23 12:48:06 +0200

    -- Schemas for user  --

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter} from "@auth/prisma-adapter";
-import { PrismaClient} from "@prisma/client";
 import authConfig from "@/auth.config";
+import {prisma} from "@/lib/prisma";
 
-const prisma = new PrismaClient();
+
 
 export const { handlers: {GET, POST}, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
