9d1dd61d3023adc51c5b2709846914115c8d8ca9
Juantrevi <juantrevi70@gmail.com>
2024-06-24 15:29:36 +0200

    -- Logout & decryoting jwt  --

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
 import {Button} from "@nextui-org/react";
 import {FaRegSmile} from "react-icons/fa";
 import Link from "next/link";
+import {auth, signOut} from "@/auth";
 
 
-export default function Home() {
+export default async function Home() {
+
+    const session = await auth();
+
   return (
       <div>
           <h1 className='text-3xl text-amber-400'>Hello!</h1>
+
+          <h3 className='text-2xl font-semibold'>User session data:</h3>
+          {session ? (
+              <div>
+                  <pre>{JSON.stringify(session, null, 2)}</pre>
+                  <form action={async () => {
+                      'use server'
+                      await signOut();
+                  }}>
+                      <Button
+                          type={'submit'}
+                          color='primary'
+                          variant='bordered'
+                          startContent={<FaRegSmile size={20} />} >
+                          Sign out
+                      </Button>
+                  </form>
+              </div>
+          ) : (
+                <div>
+                    <p>Not signed in</p>
+                </div>
+          )};
         <Button
             as={Link}
             href='/members'
