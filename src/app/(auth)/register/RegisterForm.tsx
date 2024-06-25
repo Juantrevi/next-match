805fe6f31e6017df6788a5b423f5e5ee2bac5ee6
Juantrevi <juantrevi70@gmail.com>
2024-06-25 16:32:44 +0200

    -- Populating DB and complete schema.prisma --

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -9,17 +9,13 @@ import {Input} from "@nextui-org/input";
 import {Button} from "@nextui-org/react";
 import {registerSchema, RegisterSchema} from "@/lib/schemas/registerSchema";
 import {registerUser} from "@/app/actions/authActions";
-import {any} from "zod";
 import {toast} from "react-toastify";
 import {useRouter} from "next/navigation";
 
-// Define the RegisterForm component
+
 export default function RegisterForm() {
     const router = useRouter();
 
-    // Use the useForm hook from react-hook-form to manage your form state
-    // The form validation mode is set to 'onTouched', which means validation is triggered when the input loses focus
-    // Note: The resolver for zod schema validation is commented out
     const {
         register,
         handleSubmit,
@@ -30,30 +26,21 @@ export default function RegisterForm() {
         mode: 'onTouched'
     });
 
-    // Define the onSubmit function that will be called when the form is submitted
-    // This function takes the form data as a parameter
     const onSubmit = async (data: RegisterSchema) => {
-        // Call the registerUser function with the form data
-        // This function is asynchronous, so we wait for it to complete using await
+
         const result = await registerUser(data);
 
-        // If the registration was successful, log a success message to the console
         if(result.status === 'success'){
-            toast.success('User registered successfully'); // Display a success toast message
-            router.push('/login'); // Redirect the user to the login page
+            toast.success('User registered successfully');
+            router.push('/login');
             console.log('User registered successfully');
         } else {
-            // If the registration was not successful, check if the error is an array
             if (Array.isArray(result.error)) {
-                // If the error is an array, loop through each error
                 result.error.forEach((e) => {
-                    // For each error, get the field name that caused the error
                     const fieldName = e.path.join('.') as 'email' | 'password' | 'name';
-                    // Set an error message for that field in the form
                     setError(fieldName, {message: e.message})
                 })
             }else {
-                // If the error is not an array, set a general server error message in the form
                 setError('root.serverError', {message: result.error})
             }
         }
