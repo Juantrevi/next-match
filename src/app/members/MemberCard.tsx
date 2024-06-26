caeefd17abec3975082f64e3ab2f988cf097a168
Juantrevi <juantrevi70@gmail.com>
2024-06-26 16:42:50 +0200

    -- Adding age & bg-dark-gradient --

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Member} from ".prisma/client";
 import {Card, CardFooter, Image} from "@nextui-org/react";
+import Link from "next/link";
+import {calculateAge} from "@/lib/util";
 
 type Props = {
     member: Member
@@ -8,16 +10,21 @@ type Props = {
 
 export default function MemberCard({member}: Props) {
     return (
-        <Card fullWidth>
+        <Card
+            fullWidth
+            as={Link}
+            href={`/members/${member.userId}`}
+            isPressable
+        >
             <Image
             isZoomed
             alt={member.name}
             width={300}
             src={member.image || '/images/user.png'}
             className='aspect-square object-cover'/>
-            <CardFooter>
+            <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
                 <div className='flex flex-col text-white'>
-                    <span className='font-semibold'>{member.name}</span>
+                    <span className='font-semibold'>{member.name}, {calculateAge(member.dateOfBirth)}</span>
                     <span className='text-sm'>{member.city}</span>
                 </div>
             </CardFooter>
