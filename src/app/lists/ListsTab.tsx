d4de7e367ffce7c53e0f19f1e81bfbefe43032cb
Juantrevi <juantrevi70@gmail.com>
2024-06-28 17:10:20 +0200

    -- Members who I liked - Liked and mutual Likes done --

---
+++ src/app/lists/ListsTab.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React, {Key} from 'react';
+import {Member} from ".prisma/client";
+import {Tab, Tabs} from "@nextui-org/tabs";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import MemberCard from "@/app/members/MemberCard";
+
+type Props = {
+    members: Member[];
+    likeIds: string[];
+}
+
+export default function ListsTab({members, likeIds}: Props) {
+
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const pathName = usePathname();
+
+    const tabs = [
+        {id: 'source', label: 'Members I have liked'},
+        {id: 'target', label: 'Members who have liked me'},
+        {id: 'mutual', label: 'Mutual likes'},
+
+    ]
+
+    function handleTabChange(key: Key) {
+        const params = new URLSearchParams(searchParams);
+        params.set('type', key.toString());
+        router.replace(`${pathName}?${params.toString()}`);
+    }
+
+    return (
+        <div className={'flex w-full flex-col mt-10 gap-5'}>
+            <Tabs
+            aria-label={'Like tabs'}
+            items={tabs}
+            color={'secondary'}
+            onSelectionChange={(key) => handleTabChange(key)}
+            >
+
+                {(item) => (
+                    <Tab key={item.id} title={item.label}>
+                        {members.length > 0 ? (
+                            <div className={'grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'}>
+                                {members.map(member => (
+                                    <MemberCard key={member.id} member={member} likeIds={likeIds} />
+                                ))}
+
+                            </div>
+                        ) : (
+                            <div>No Members for this filter</div>
+                        )}
+                    </Tab>
+                )}
+
+            </Tabs>
+        </div>
+    );
+};
--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ListsTab from "@/app/lists/ListsTab";
+import {fetchCurrentUserLikeIds, fetchLikedMembers} from "@/app/actions/likeActions";
 
-export default function ListPage() {
+export default async function ListsPage({searchParams}: {searchParams: {type: string}}) {
+
+    const likeIds = await fetchCurrentUserLikeIds();
+    const members = await fetchLikedMembers(searchParams.type)
+
     return (
         <div>
-            
+            <ListsTab members={members} likeIds={likeIds} />
         </div>
     );
 };
