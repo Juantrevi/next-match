0f3709f75998f8bb1f2cd450c37f46ef860ee955
Juantrevi <juantrevi70@gmail.com>
2024-06-28 16:14:38 +0200

    -- Like function done (Remaining logic for users) --

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -6,7 +6,7 @@ import {prisma} from "@/lib/prisma";
 import {ActionResult} from "@/types";
 import {User} from "@prisma/client";
 import {LoginSchema} from "@/lib/schemas/loginSchema";
-import {signIn, signOut} from "@/auth";
+import {auth, signIn, signOut} from "@/auth";
 import {AuthError} from "next-auth";
 import {redirect} from "next/navigation";
 
@@ -96,4 +96,13 @@ export async function getUserById(id: string) {
             id
         }
     });
+}
+
+export async function getAuthUserId(){
+    const session = await auth()
+    const userId = session?.user?.id
+
+    if(!userId) throw new Error('Unauthorized');
+
+    return userId;
 }
--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -3,6 +3,7 @@ import {Member} from ".prisma/client";
 import {Card, CardFooter, Image} from "@nextui-org/react";
 import Link from "next/link";
 import {calculateAge} from "@/lib/util";
+import LikeButton from "@/components/LikeButton";
 
 type Props = {
     member: Member
@@ -22,6 +23,11 @@ export default function MemberCard({member}: Props) {
             width={300}
             src={member.image || '/images/user.png'}
             className='aspect-square object-cover'/>
+
+            <div className={'absolute top-3 right-3 z-50'}>
+                <LikeButton targetId={member.userId} hasLiked={false} />
+            </div>
+
             <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
                 <div className='flex flex-col text-white'>
                     <span className='font-semibold'>{member.name}, {calculateAge(member.dateOfBirth)}</span>
