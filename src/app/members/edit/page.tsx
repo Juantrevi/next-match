61e2c8c7fd2a467896ae041b7b289e4a02a430f4
Juantrevi <juantrevi70@gmail.com>
2024-07-01 14:28:59 +0200

    -- Edit profile schema and form done (Functionality pending) --

--- src/app/members/edit/page.tsx
+++ src/app/members/edit/page.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {CardBody, CardHeader} from "@nextui-org/card";
+import {Divider} from "@nextui-org/react";
+import EditForm from "@/app/members/edit/EditForm";
+import {getAuthUserId} from "@/app/actions/authActions";
+import {getMemberByUserId} from "@/app/actions/memberActions";
+import {notFound} from "next/navigation";
 
-export default function MemberEditPage() {
+export default async function MemberEditPage() {
+
+    const userId = await getAuthUserId();
+
+    const member = await getMemberByUserId(userId);
+
+    if (!member) {
+        return notFound();
+    }
+
     return (
-        <div>
-            Member Edit Page
-        </div>
+        <>
+            <CardHeader className={'text-2xl font-semibold text-secondary'}>
+                Edit Profile
+            </CardHeader>
+            <Divider/>
+            <CardBody>
+                <EditForm member={member} />
+            </CardBody>
+        </>
     );
 };
 
