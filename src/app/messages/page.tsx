6fc1af6d3f7d1096d6d5bd8d71648a1921daff36
Juantrevi <juantrevi70@gmail.com>
2024-07-05 19:04:52 +0200

    -- MessageSidebar structure design --

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import MessageSidebar from "@/app/messages/MessageSidebar";
 
 export default function MessagesPage() {
     return (
-        <div>
-            
+        <div className={'grid grid-cols-12 gap-5 height-[80vh] mt-10'}>
+            <div className={'col-span-2'}>
+                <MessageSidebar />
+            </div>
+            <div className={'col-span-10'}>
+                Message table goes here
+            </div>
         </div>
     );
 };
