0b80f6b532a8fec828e253b1f630cb28733404d8
Juantrevi <juantrevi70@gmail.com>
2024-07-08 12:53:19 +0200

    -- MessageSidebar structure design --

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import MessageSidebar from "@/app/messages/MessageSidebar";
+import {getMessagesByContainer, getMessageThread} from "@/app/actions/messageActions";
 
-export default function MessagesPage() {
+export default async function MessagesPage({searchParams}:{searchParams: {container: string}}) {
+
+    const messages = await getMessagesByContainer(searchParams.container);
+
     return (
         <div className={'grid grid-cols-12 gap-5 height-[80vh] mt-10'}>
             <div className={'col-span-2'}>
