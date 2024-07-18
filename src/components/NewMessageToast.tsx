a50e50582447bb3baa2dadb9d24a0d396ec41b0b
Juantrevi <juantrevi70@gmail.com>
2024-07-18 16:00:52 +0200

    -- NotificationToast to user when they're liked --

--- src/components/NewMessageToast.tsx
+++ src/components/NewMessageToast.tsx
@@ -33,6 +33,6 @@ export default function NewMessageToast({message}: Props) {
     );
 };
 
-export const newMessageToast = (message: MessageDto) => {
-    toast(<NewMessageToast message={message}/>)
-};
+// export const newMessageToast = (message: MessageDto) => {
+//     toast(<NewMessageToast message={message}/>)
+// };
