a83064108ab88584d8a60dd06e20132a86a16f67
Juantrevi <juantrevi70@gmail.com>
2024-07-29 23:59:59 +0200

    -- Filtering by gender --

--- src/hooks/useFilters.ts
+++ src/hooks/useFilters.ts
@@ -1,5 +1,5 @@
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {FaFemale, FaMale} from "react-icons/fa";
 import {Selection} from "@nextui-org/react";
 import useFilterStore from "@/hooks/useFilterStore";
@@ -9,7 +9,6 @@ export const useFilters = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
     const [clientLoaded, setClientLoaded] = useState(false)
-    const selectedGender = searchParams.get('gender')?.split(',') || ['male', 'female'];
 
     const orderByList = [
         {label: 'Last active', value: 'updated'},
@@ -20,33 +19,36 @@ export const useFilters = () => {
 
     const {gender, ageRange, orderBy} = filters;
 
+    useEffect(() => {
+        const searchParams = new URLSearchParams();
+
+        if(gender) searchParams.set('gender', gender.join(','));
+        if(ageRange) searchParams.set('ageRange', ageRange.toString());
+        if(orderBy) searchParams.set('orderBy', orderBy);
+
+        router.replace(`${pathname}?${searchParams}`);
+
+
+    }, [ageRange, gender, orderBy, router, pathname])
+
     const genders = [
         {value: 'male', icon: FaMale},
         {value: 'female', icon: FaFemale},
     ]
 
     const handleAgeSelect = (value: number[]) => {
-        const params = new URLSearchParams(searchParams);
-        params.set('ageRange', value.join(','));
-        router.replace(`${pathname}?${params}`);
+        setFilters('ageRange', value);
     }
 
     const handleOrderSelect = (value: Selection) => {
         if(value instanceof Set) {
-            const params = new URLSearchParams(searchParams);
-            params.set('orderBy', value.values().next().value);
-            router.replace(`${pathname}?${params}`);
+            setFilters('orderBy', value.values().next().value);
+
         }
     }
 
     const handleGenderSelect = (value: string) => {
-        const params = new URLSearchParams(searchParams);
-        if(selectedGender.includes(value)) {
-            params.set('gender', selectedGender.filter(g => g !== value).toString())
-        }else{
-            params.set('gender', [...selectedGender, value].toString())
-        }
-        router.replace(`${pathname}?${params}`);
-
+        if(gender.includes(value)) setFilters('gender', gender.filter(g => g !== value));
+        else setFilters('gender', [...gender, value]);
     }
 }
