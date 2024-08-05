1c5220bd9844b78f2678a76314c2d902b16011a5
Juantrevi <juantrevi70@gmail.com>
2024-08-05 16:21:09 +0200

    -- Onboarding process [1] --

--- src/hooks/usePresenceChannel.ts
+++ src/hooks/usePresenceChannel.ts
@@ -4,7 +4,7 @@ import {Channel, Members} from "pusher-js";
 import {pusherClient} from "@/lib/pusher";
 import {updateLastActive} from "@/app/actions/memberActions";
 
-export const usePresenceChannel = () => {
+export const usePresenceChannel = (userId: string | null) => {
     const {set, add, remove} = usePresenceStore(state => ({
         set: state.set,
         add: state.add,
@@ -25,6 +25,7 @@ export const usePresenceChannel = () => {
     }, [remove])
 
     useEffect(() => {
+        if (!userId) return;
         if(!channelRef.current){
             channelRef.current = pusherClient.subscribe('presence-nm');
 
@@ -51,6 +52,6 @@ export const usePresenceChannel = () => {
             }
         }
 
-    }, [handleSetMembers, handleAddMember, handleRemoveMember])
+    }, [handleSetMembers, handleAddMember, handleRemoveMember, userId])
 
 }
--- src/lib/schemas/registerSchema.ts
+++ src/lib/schemas/registerSchema.ts
@@ -1,4 +1,5 @@
 import {z} from 'zod';
+import {calculateAge} from "@/lib/util";
 
 export const registerSchema = z.object({
     name: z.string().min(3, {
@@ -11,5 +12,20 @@ export const registerSchema = z.object({
         message: 'Password must be at least 6 characters long',
     }),
 });
+
+export const profileSchema = z.object({
+    gender: z.string().min(1),
+    description: z.string().min(1),
+    city: z.string().min(1),
+    country: z.string().min(1),
+    dateOfBirth: z.string().min(1, {
+        message: 'Date of birth is required',
+    }).refine(dateString => {
+        const age = calculateAge(new Date(dateString));
+        return age >= 18;
+    }, {
+        message: 'You must be at least 18 years old',
+    }),
+})
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
