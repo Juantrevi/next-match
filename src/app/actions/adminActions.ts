f94bf362c9101a43a56ba297d108a082dc9d0cec
Juantrevi <juantrevi70@gmail.com>
2024-08-21 15:35:57 -0300

    -- Admin photo approval [3] --

--- src/app/actions/adminActions.ts
+++ src/app/actions/adminActions.ts
@@ -2,6 +2,8 @@
 
 import {getUserRole} from "@/app/actions/authActions";
 import {prisma} from "@/lib/prisma";
+import {Photo} from ".prisma/client";
+import {cloudinary} from "@/lib/cloudinary";
 
 export async function getUnapprovedPhotos() {
     try {
@@ -19,5 +21,67 @@ export async function getUnapprovedPhotos() {
         console.log(error);
         throw error;
 
+    }
+}
+
+export async function approvePhoto(photoId: string){
+    try {
+            const role = await getUserRole();
+
+            if(role != 'ADMIN') throw new Error('Forbidden');
+
+            const photo = await prisma.photo.findUnique({
+                where: {id: photoId},
+                include: {member: {include: {user: true}}}
+            });
+
+            if(!photo || !photo.member || !photo.member.user) throw new Error('Cannot approve this image');
+
+            const {member} = photo;
+
+            const userUpdate = member.user && member.user.image == null ? {image: photo.url} : {};
+            const memberUpdate = member.image === null ? {image: photo.url} : {};
+
+            if(Object.keys(userUpdate).length > 0){
+                await prisma.user.update({
+                    where: {id: member.userId},
+                    data: userUpdate
+                })
+            }
+
+            return prisma.member.update({
+                where: {id: member.id},
+                data: {
+                    ...memberUpdate,
+                    photos: {
+                        update: {
+                            where: {id: photo.id},
+                            data: {isApproved: true}
+                        }
+                    }
+                }
+            })
+    }catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function rejectPhoto(photo: Photo){
+    try {
+        const role = await getUserRole();
+
+        if(role != 'ADMIN') throw new Error('Forbidden');
+
+        if(photo.publicId){
+            await cloudinary.v2.uploader.destroy(photo.publicId);
+        }
+
+        return prisma.photo.delete({
+            where: {id: photo.id}
+        })
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }
