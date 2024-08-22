6aea0ac9e1a58a912944354483f5c2ffa2766bd8
Juantrevi <juantrevi70@gmail.com>
2024-08-22 16:53:25 -0300

    -- Loading spinner on the whole app --

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,45 @@
 import {auth, signOut} from "@/auth";
-import ClientSession from "@/components/ClientSession";
-
+import {GiMatchTip} from "react-icons/gi";
+import {Button} from "@nextui-org/react";
+import Link from "next/link";
 
 export default async function Home() {
 
     const session = await auth();
 
   return (
-      <div className={'flex flex-row justify-around mt-20 gap-6'}>
-          <div className={'bg-green-50 p-10 rounded-xl shadow-md w-1/2 overflow-auto'}>
-              { session ? (
-                  <div>
-                      <h3 className='text-2xl font-semibold'>Server session data: </h3>
-                      <pre>{ JSON.stringify(session, null, 2) }</pre>
-                  </div>
-              ) : (
-                  <div>
-                      <p>Not signed in</p>
-                  </div>
-              )}
-          </div>
-          <ClientSession />
+      <div className={'flex flex-col justify-center items-center mt-20 gap-6 text-secondary'}>
+        <GiMatchTip size={100}/>
+          <h1 className={'text-4xl font-bold'}>Welcome to NextMatch</h1>
+          {session ? (
+           <Button
+               as={Link}
+               href={'/members'}
+               size={'lg'}
+               color={'secondary'}
+               variant={'bordered'}>
+               Continue
+           </Button>
+          ): (
+              <div className={'flex flex-row gap-4'}>
+                  <Button
+                      as={Link}
+                      href={'/login'}
+                      size={'lg'}
+                      color={'secondary'}
+                      variant={'bordered'}>
+                        Sign in
+                  </Button>
+                  <Button
+                      as={Link}
+                      href={'/register'}
+                      size={'lg'}
+                      color={'secondary'}
+                      variant={'bordered'}>
+                        Register
+                  </Button>
+              </div>
+          )}
       </div>
   );
 }
